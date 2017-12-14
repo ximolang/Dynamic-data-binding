f1ab35eac3ca006259d1c2e16ef38f78a4b797ca
ximolang-PC <txiliang@163.com>
2017-12-14 17:32:05 +0800

    ...

--- Observer.js
+++ Observer.js
@@ -5,42 +5,12 @@ let isObject = (obj) => {
   return false
 }
 
-let def = (obj, prop, ctx) => {
-  obj[prop] = ctx.data
-}
-
-// 尝试创建observe实例，如果已经动态化则返回，没有则new一个
-let observe = (value) => {
-  if (!isObject(value)) return false
-
-  let _ob = null
-  if (value.hasOwnProperty('__ob__') && value.__ob__ instanceof Observer) {
-    _ob = value.__ob__
-  } else {
-    // 假使value为简单对象，而不是函数，正则之类
-    _ob = new Observer(value)
-  }
-
-  return _ob
-}
-
-let defineReactive = (obj, key, val) => {
-  let ob = observe(val)
-
+let def = (obj, key, val, enumerable) => {
   Object.defineProperty(obj, key, {
-    enumerable: true,
-    configurable: true,
-    get() {
-      log('你访问了' + val)
-      return val
-    },
-    set(newVal) {
-      log('你设置了' + key + ', 新的值为' + newVal)
-      val = newVal
-      ob = observe(newVal)
-      // 执行监视回调
-      // self.event.emit(key, val)
-    },
+    value: val,
+    enumerable: !!enumerable,
+    writable: true,
+    configurable: true
   })
 }
 
@@ -66,26 +36,21 @@ class Event {
     }
   }
 }
-
+let eventBus = new Event()
 class Observer {
   constructor (data) {
     this.data = data
     // $watch监视数据变化的函数列表
-    this.event = new Event()
+    this.event = eventBus
     // 定义标识来判断该对象是否已经被动态化
-    def(this.data, '__ob__', this)
-
-    this.walk(this.data)
+    def(data, '__ob__', this, false)
+    this.walk(data)
   }
 
   walk(value) {
-    log('!!!!! ->' + value)
-  
     let array = Object.keys(value)
-    log(array)
     for (let i = 0; i < array.length; i++) {
-      if (array[i] === '__ob__') continue
-      defineReactive(value, array[i], value[array[i]])
+      defineReactive(value, array[i], value[array[i]], this.event)
     }
   }
 
@@ -95,17 +60,36 @@ class Observer {
   }
 }
 
-let data = {
-  name: 'Molang',
-  age: 22,
-  skills: {
-    code: 'js',
-    life: 'painting',
-  },
+// helper
+
+// 尝试创建observe实例，如果已经动态化则返回，没有则new一个
+function observe (value) {
+  if (!isObject(value)) return false
+  let _ob = null
+  if (value.hasOwnProperty('__ob__') && value.__ob__ instanceof Observer) {
+    _ob = value.__ob__
+  } else {
+    // 假使value为简单对象，而不是函数，正则之类
+    _ob = new Observer(value)
+  }
+  return _ob
 }
 
-let app = new Observer(data)
-
-app.$watch('skills', (name) => {
-  log('我的姓名变了！变成' + name + '了!')
-})
+function defineReactive (obj, key, val, eventBus) {
+  let ob = observe(val)
+  Object.defineProperty(obj, key, {
+    enumerable: true,
+    configurable: true,
+    get() {
+      log('你访问了' + key)
+      return val
+    },
+    set(newVal) {
+      log('你设置了' + key + ', 新的值为' + newVal)
+      val = newVal
+      ob = observe(newVal)
+      // 执行监视回调
+      eventBus.emit(key, newVal)
+    },
+  })
+}
